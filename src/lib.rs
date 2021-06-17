57a8c1b7c5e9bcc3d92c75732c29ba7300f40753
regenerativep <regeneralp@gmail.com>
2021-06-17 02:31:38 -0400

    1.17

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,23 @@ extern crate alloc;
 #[cfg(all(test, feature = "std", feature = "bench"))]
 extern crate test;
 
+pub mod byte_order;
+mod chat;
 mod deserialize;
-mod serialize;
 pub mod nbt;
 pub mod protocol;
+mod serialize;
 pub mod status;
 pub mod types;
 pub mod utils;
 pub mod uuid;
-mod chat;
-pub mod byte_order;
 
 #[cfg(feature = "v1_15_2")]
 pub mod v1_15_2;
 #[cfg(feature = "v1_16_3")]
 pub mod v1_16_3;
+#[cfg(feature = "v1_17_0")]
+pub mod v1_17_0;
 
 pub use deserialize::*;
 pub use serialize::*;
